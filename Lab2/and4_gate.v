8ecb109a5131ec3030f48bf03858e0b276cd86b6
Andra Pescaru <87182577+AndraPescaru@users.noreply.github.com>
2022-03-08 22:56:01 +0200

    Update and4_gate.v

--- Lab2/and4_gate.v
+++ Lab2/and4_gate.v
@@ -1,50 +1,31 @@
 `timescale 1ns / 1ps
 
-module and4_gate_tb();
-
-reg in0_tb;
-reg in1_tb;
-reg in2_tb;
-reg in3_tb;
-wire out0_tb;
-
-and4_gate dut(
-    .in0(in0_tb),
-    .in1(in1_tb),
-    .in2(in2_tb),
-    .in3(in3_tb),
-    .out0(out0_tb)
-);
-
-initial 
-begin
-    in0_tb = 0;
-    in1_tb = 1;
-    in2_tb = 0;
-    in3_tb = 0;
-    #5 in3_tb = 1;
-    #5 in3_tb = 0;
-    #5 in3_tb = 1;
-    #5 in3_tb = 0;
-    #5 in3_tb = 1;
-    #5 in3_tb = 0;
-    #5 in3_tb = 1;
-       in2_tb = 1;
-       in0_tb = 1;
-    #5 in3_tb = 0;
-    #5 in3_tb = 1;
-    #5 in3_tb = 0;
-       in2_tb = 0;
-       in0_tb = 0;
-    #5 in3_tb = 1;
-    #5 in3_tb = 0;
-    #5 in3_tb = 1;
-       in2_tb = 1;
-    #5 in3_tb = 0;
+module and4_gate(
+        input in0,
+        input in1,
+        input in2,
+        input in3,
+        output out0
+    );
     
-   #20 $stop();
-   end
+    wire w0, w1;
     
+    and2_gate and2_gate_0(
+        .in0(in0),
+        .in1(in1),
+        .out0(w0)
+    );
     
-
+    and2_gate and2_gate_1(
+        .in0(in2),
+        .in1(in3),
+        .out0(w1)
+        
+    );
+    
+    and2_gate and2_gate_2(
+        .in0(w0),
+        .in1(w1),
+        .out0(out0)
+    );
 endmodule
