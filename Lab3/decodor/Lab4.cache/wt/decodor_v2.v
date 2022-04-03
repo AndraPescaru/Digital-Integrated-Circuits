7bcb46c8f3a8580caf6f7df42798fa06b3ab5bff
Andra Pescaru <87182577+AndraPescaru@users.noreply.github.com>
2022-04-03 18:15:57 +0300

    Create decodor_v2.v

---
+++ Lab3/decodor/Lab4.cache/wt/decodor_v2.v
@@ -0,0 +1,13 @@
+module decodor_v2
+	(
+		input wire [1:0] in,
+		output wire [3:0] out 
+	);
+	
+	// fiecare bit din iesire calculat independent
+assign out[0] = ~in[0] & ~in[1];
+assign out[1] = in[0] & ~in[1];
+assign out[2] = ~in[0] & in[1];
+assign out[3] = in[0] & in[1];
+
+endmodule
